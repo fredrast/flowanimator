8b00a10b37977fd91b8dca5b6bf2a277388d4efe
fredrast <Fredrik.Astrom@iki.fi>
2019-10-23 17:18:16 +0300

    Try fetching 1000 items at a time from REST API

--- src/jira.js
+++ src/jira.js
@@ -2,11 +2,17 @@ export function getBoardsFromJira(serverUrl, id, token) {
   const BOARDS_PATH = '/rest/agile/1.0/board';
   const boardsUrl = serverUrl + BOARDS_PATH;
   // const boardsPromise = fetchFromJira(boardsUrl, id, token, {});
+
+  const parameters = {
+    startAt: 0,
+    maxResults: 500,
+  };
+
   const boardsPromise = recursiveFetchFromJira(
     boardsUrl,
     id,
     token,
-    {}, // parameters
+    parameters,
     0, // startAt
     'values', // fieldName
     [] // values
@@ -29,6 +35,7 @@ export function getIssuesFromJira(serverUrl, id, token, filterID) {
   const parameters = {
     jql: 'filter = ' + filterID,
     startAt: 100,
+    maxResults: 500,
     fields: ['key', 'summary', 'created', 'status', 'assignee'],
     expand: 'changelog',
   };
@@ -63,20 +70,11 @@ function recursiveFetchFromJira(
   fieldName,
   values
 ) {
-  console.log('recursiveFetchFromJira starting at ' + startAt);
-  console.log('Values so far:');
-  console.log(values);
+
   parameters['startAt'] = startAt;
 
   const valuesPromise = fetchFromJira(url, id, token, parameters).then(
     response => {
-      console.log('Response received:');
-      console.log(response);
-      console.log(response['startAt']);
-      console.log(fieldName);
-      console.log(response[fieldName]);
-      console.log(response[fieldName].length);
-      console.log('');
       if (response[fieldName].length > 0) {
         return recursiveFetchFromJira(
           url,
@@ -100,6 +98,8 @@ function recursiveFetchFromJira(
  ****************************************************************************/
 
 function fetchFromJira(url, id, token, parameters) {
+
+
   const authorizationString = 'Basic ' + btoa(id + ':' + token);
   const options = {
     method: 'GET',
@@ -109,9 +109,15 @@ function fetchFromJira(url, id, token, parameters) {
     },
   };
 
+console.log('######################################## OPTIONS ########################################');
+  console.log(options);
+
   if (parameters) {
     url = url + '?' + serialize(parameters);
   }
+
+    console.log('Fetching from url:');
+  console.log(url);
 
   const resultPromise = fetch(url, options)
     .then(response => {
