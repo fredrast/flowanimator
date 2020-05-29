832bd42202cdc843bdbcb6cade135daf8e54143c
Fredrik Åström <fredrik.astrom@iki.fi>
2020-05-29 18:42:22 +0300

    Remove test files

--- test/test.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('mousemove', e => {
-  const token = document.getElementById('OFI-12345');
-  token.style.left = e.clientX + 'px';
-  token.style.top = e.clientY + 'px';
-});
