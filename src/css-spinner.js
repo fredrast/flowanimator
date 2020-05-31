b849daa61835ef00272b9eab9ba9f046179ba9ee
Fredrik Åström <fredrik.astrom@iki.fi>
2020-05-31 21:29:17 +0300

    Fix class --> className in css-spinner.js

--- src/css-spinner.js
+++ src/css-spinner.js
@@ -4,13 +4,13 @@ import './css-spinner.css';
 export default function CssSpinner(props) {
   if (props.visible) {
     return (
-      <div class="sk-fading-circle">
-        <div class="sk-circle1 sk-circle" />
-        <div class="sk-circle2 sk-circle" />
-        <div class="sk-circle3 sk-circle" />
-        <div class="sk-circle4 sk-circle" />
-        <div class="sk-circle5 sk-circle" />
-        <div class="sk-circle6 sk-circle" />
+      <div className="sk-fading-circle">
+        <div className="sk-circle1 sk-circle" />
+        <div className="sk-circle2 sk-circle" />
+        <div className="sk-circle3 sk-circle" />
+        <div className="sk-circle4 sk-circle" />
+        <div className="sk-circle5 sk-circle" />
+        <div className="sk-circle6 sk-circle" />
       </div>
     );
   } else {
